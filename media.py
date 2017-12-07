d24d38902e50c8901e221b2aefd5ee405a64ca4c
hamidkreaa <hamidkreaa@hotmail.com>
2017-12-07 15:31:38 +0100

    Add  comments

--- media.py
+++ media.py
@@ -2,12 +2,14 @@ import webbrowser
 
 
 class Movie():
+    """ the Class Movie is Model for the Movie objects
     def __init__(self, movie_title, movie_storyline, poster_image,
-    trailer_youtube):
+                 trailer_youtube):
         self.title = movie_title
         self.storyline = movie_storyline
         self.poster_image_url = poster_image
         self.trailer_youtube_url = trailer_youtube
 
+""" this method opens  trailer for the movie using trailer_youtube_url
     def show_trailer(self):
         webbrowser.open(self.trailer_youtube_url)
