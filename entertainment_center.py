5117932f1fc096e0a6b909f3a6019952192ff0b6
hamidkreaa <hamidkreaa@hotmail.com>
2017-12-07 15:20:26 +0100

    Remove Unnecesery Code

--- entertainment_center.py
+++ entertainment_center.py
@@ -8,18 +8,11 @@ toy_story = media.Movie(
     "https://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg",
     "https://www.youtube.com/watch?v=KYz2wyBy3kc")
 
-# print(toy_story.storyline)
-# print '-' * 30
-
 # Create instance for movie Avatar
 avatar = media.Movie(
     "Avatar", " A marine on an alien planet",
     "https://upload.wikimedia.org/wikipedia/en/b/b0/Avatar-Teaser-Poster.jpg",
     "https://www.youtube.com/watch?v=uZNHIU3uHT4")
-
-# print(avatar.storyline)
-# avatar.show_trailer()
-# print '-' * 30
 
 # Create instance for movie Fury
 fury = media.Movie(
@@ -27,19 +20,11 @@ fury = media.Movie(
     "https://upload.wikimedia.org/wikipedia/en/1/17/Fury_2014_poster.jpg",
     "https://www.youtube.com/watch?v=-OGvZoIrXpg")
 
-# print(fury.storyline)
-# fury.show_trailer()
-# print '-' * 30
-
 # Create instance for movie Hunger Games
 hunger_games = media.Movie(
     "Hunger Games", " A really real hunger game show",
     "https://upload.wikimedia.org/wikipedia/en/4/42/HungerGamesPoster.jpg",
     "https://www.youtube.com/watch?v=4S9a5V9ODuY")
-
-# print(hunger_games.storyline)
-# hunger_games.show_trailer()
-# print '-' * 30
 
 # Create instance for movie Pretty Woman
 pretty_women = media.Movie(
@@ -47,18 +32,11 @@ pretty_women = media.Movie(
     "https://upload.wikimedia.org/wikipedia/en/b/b6/Pretty_woman_movie.jpg",
     "https://www.youtube.com/watch?v=Wzii8IuL8lk")
 
-# print(pretty_women.storyline)
-# pretty_women.show_trailer()
-# print '-' * 30
-
 # Create instance for movie Ghost Busters
 ghost_busters = media.Movie(
     "Ghost Busters", " An action movie about Ghosts hunting",
     "https://upload.wikimedia.org/wikipedia/en/3/32/Ghostbusters_2016_film_poster.png",  # NOQA
     "https://www.youtube.com/watch?v=w3ugHP-yZXw")
-
-# print(ghost_busters.storyline)
-# ghost_busters.show_trailer()
 
 # make a list of movies instences
 movies = [toy_story, avatar, fury, hunger_games, pretty_women, ghost_busters]
