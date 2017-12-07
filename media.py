89aaa3ed330624b5d639d57c58deb0cd7deebbaa
hamidkreaa <hamidkreaa@hotmail.com>
2017-12-07 16:41:18 +0100

    Fix Reviewers note

--- media.py
+++ media.py
@@ -2,14 +2,15 @@ import webbrowser
 
 
 class Movie():
-    """ the Class Movie is Model for the Movie objects
     def __init__(self, movie_title, movie_storyline, poster_image,
                  trailer_youtube):
+        """ the Class Movie is Model for the Movie objects """
         self.title = movie_title
         self.storyline = movie_storyline
         self.poster_image_url = poster_image
         self.trailer_youtube_url = trailer_youtube
 
-""" this method opens  trailer for the movie using trailer_youtube_url
     def show_trailer(self):
-        webbrowser.open(self.trailer_youtube_url)
+    """ this method opens
+        trailer for the movie using trailer_youtube_url """
+    webbrowser.open(self.trailer_youtube_url)
